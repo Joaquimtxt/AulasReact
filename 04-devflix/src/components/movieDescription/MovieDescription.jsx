57ce34045fa0c589bc3435246026288f9dec773b
Joaquim <jocaq124@gmail.com>
2025-03-18 14:21:11 -0300

    Ajustes no MovieDescription

--- 04-devflix/src/components/movieDescription/MovieDescription.jsx
+++ 04-devflix/src/components/movieDescription/MovieDescription.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import styles from "./MovieDescription.module.css"
+import devFlix from "/favicon.png"
 
 const MovieDescription = (props) => {
     const [movieDesc, setMovieDesc] = useState([]);
@@ -9,17 +10,49 @@ const MovieDescription = (props) => {
         .then((data)=>setMovieDesc(data))
         .catch((error)=> console.error(error));
     }, [])
-  return (
-<div className={styles.modalBackdrop} onClick={props.click}>
-<div className={styles.movieModal} onClick={(e)=> e.stopPropagation()}>
-    <div className={styles.movieInfo}>
-        <img src={movieDesc.Poster} alt="" />
-        <button onClick={props.click} className={styles.btnClose}>X</button>
-    </div>
-</div>
-</div>
-
-  )
-}
+    return (
+      <div className={styles.modalBackdrop} onClick={props.click}>
+        <div className={styles.movieModal} onClick={(e) => e.stopPropagation()}>
+          <div className={styles.movieInfo}>
+            <img
+              src={movieDesc.Poster}
+              alt={`Imagem da capa do filme ${movieDesc.Title}`}
+            />
+            <button className={styles.btnClose} onClick={props.click}>
+              X
+            </button>
+            <div className={styles.movieType}>
+              <div>
+                <img src={devFlix} alt="" />
+                {movieDesc.Type}
+                <h1>{movieDesc.Title}</h1>
+                <a
+                  href={`https://google.com/search?q=${encodeURIComponent(
+                    movieDesc.Title
+                  )}`}
+                  target="_blank"
+                >
+                  ▶️ Assistir
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className={styles.containerMisc}>
+            <div className={styles.containerFlex}>
+              Avaliação: {movieDesc.imdbRating} | Duração: {movieDesc.Runtime} |{" "}
+              {movieDesc.Released}
+            </div>
+            <div className={styles.containerFlex}>
+              <p>Elenco: {movieDesc.Actors}</p>
+              <p>Gênero: {movieDesc.Genre}</p>
+            </div>
+          </div>
+          <div className={styles.desc}>
+            <p>Sinopse: {movieDesc.Plot}</p>
+          </div>
+        </div>
+      </div>
+    );
+  };
 
 export default MovieDescription
