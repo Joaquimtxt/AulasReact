b3ab93965cb0e7f3991a97d24813c228ec8e33aa
Joaquim <jocaq124@gmail.com>
2025-03-21 00:10:19 -0300

    Terminado responsividade e feito alguns hovers do footer

--- 05-portfolio/components/Footer/Footer.jsx
+++ 05-portfolio/components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import styles from './Footer.module.css'
 const Footer = (props) => {
     return (
         <footer className={styles.Footer}>
-          <div>
-            <p>Feito com ❤️ por <a href={props.devLink} target='_blank' rel='noopener noreferrer'>{props.devName}</a></p>
+          <div className={styles.FooterItems}>
+            <p>Feito com ❤️ por <a href={props.devLink} className={styles.devLink} target='_blank' rel='noopener noreferrer'>{props.devName}</a></p>
           </div>
           <div className={styles.IconsItems}>
             <a className={styles.Icon} href={props.Link1} target="_blank" rel='noopener noreferrer'>
