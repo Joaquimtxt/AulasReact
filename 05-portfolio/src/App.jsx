fc43b374bc80132c0e76cd858b4558a1ecabe827
Joaquim <jocaq124@gmail.com>
2025-03-28 00:26:26 -0300

    Feito o texto do Sobre Mim

--- 05-portfolio/src/App.jsx
+++ 05-portfolio/src/App.jsx
@@ -7,7 +7,14 @@ import Photo from "./img/JOca.png";
 import "./App.css";
 
 function App() {
-   
+   const Data = new Date();
+    const Nascimento = new Date(2007, 8, 10);
+    var Idade = Data.getFullYear() - Nascimento.getFullYear();
+    const MesAtual = Data.getMonth()-1;
+   const DiaAtual = Data.getDate();
+   if (MesAtual < Nascimento.getMonth()-1 || (MesAtual === Nascimento.getMonth() && DiaAtual < Nascimento.getDate())) {
+    Idade--;
+}
   return (
     <div id="App">
       <div id="container">
@@ -23,7 +30,8 @@ function App() {
           <About
           titulo="Sobre Mim"
             devNome="Oi meu nome é Joaquim Guilherme"
-            descricao="Tenho 17 anos."
+            descricao={`Tenho ${Idade} anos, comecei meus estudos em 2024 no Senai em Jaú, onde aprendi a fazer códigos em html, css, e programar em JavaScript, C#, React, e SQL para análise de banco de dados. Sou apaixonado  também pelo estudo de línguas estrangeiras, conseguindo ter conversações avançadas em inglês, e básicas em italiano, também sei ler o alfabeto russo. Sou uma pessoa que gosta de aprender, e sempre busco novos desafios. Estou sempre em busca de novos conhecimentos, e me aprimorando cada vez mais. Gosto de trabalhar em equipe, e estou sempre disposto a ajudar os outros, e a aprender com eles.
+             Abaixo é possível ter a ideia de alguns projetos já desenvolvidos por mim, e das tecnologias utilizadas. `}
           />
           <Portfolio />
           <Linguagens titulo="Tecnologias utilizadas" />
