63593f2d6b494fd9bc6c503d775ea38e6f399d7f
Joaquim <jocaq124@gmail.com>
2025-04-08 17:02:22 -0300

    Feito Mudanças significativas no Chat, deixado praticamente pronto, amanhã o professor terminará junto de nós

--- 07-devChat/src/Pages/Chat.jsx
+++ 07-devChat/src/Pages/Chat.jsx
@@ -1,35 +1,78 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
 
 const Chat = (props) => {
-    //Fake message data
-    const messages = [
-        {authorId: 1, author:"John Doe", text:"Hello, how are you?"},
-        {authorId: 2, author:"Jane Doe", text:"I'm fine, thank you! And you?"},
-        {authorId: 3, author:"Lucas Doe", text:"I'm good too!"},
-    ]
+  const [messageList, setMessageList] = useState([]);
+  const messageRef = useRef();
+  const bottomRef = useRef();
+
+  useEffect(() => {
+    props.socket.on("receive_message", (data) => {
+      setMessageList((current) => [...current, data]);
+    });
+    return () => props.socket.off("receive_message");
+  }, [props.socket]);
+
+  useEffect(() => {
+  bottomRef.current.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+    });
+  }, [messageList]);
+
+  const handleSubmit = () => {
+    const message = messageRef.current.value;
+    if (!message.trim()) return;
+
+    props.socket.emit("message", message)
+
+    messageRef.current.value = "";
+    messageRef.current.focus();
+  };
+
   return (
-    <div id="chat-container" style={{width: "400px", height: "600px"}} className='bg-secondary rounded-4 p-3 d-flex flex-column m-4'>
-        <div id="chat-body" className='overflow-y-hidden h-100 d-flex flex-column gap-3'>
-{messages.map((message, index) => (
-    <div className='align-self-start bg-warning-subtle rounded-3 p-2 text-dark me-5' key={index}> 
-    <div className='fw-bold' id="message-author">{message.author}</div>
-    <div id="message-text">{message.text}</div>
+    <div
+      id="chat-container"
+      style={{ width: "400px", height: "600px" }}
+      className="bg-secondary rounded-4 p-3 d-flex flex-column m-4"
+    >
+      <div
+        id="chat-body"
+        className="overflow-y-hidden h-100 d-flex flex-column gap-3"
+      >
+        {messageList.map((message, index) => (
+          <div
+        className={`${message.authorId === props.socket.id? "align-self-end ms-5 bg-success-subtle" : "align-self-start me-5 bg-warning-subtle"}  rounded-3 p-2 text-dark text-break`}
+            key={index}
+          >
+            <div className="fw-bold" id="message-author">
+              {message.author}
+            </div>
+            <div id="message-text">{message.text}</div>
+          </div>
+        ))}
+        <div ref={bottomRef}/>{/**/}
+        </div>
+      <div id="chat-footer" className="input-group">
+        <input
+          ref={messageRef}
+          id="msgUser"
+          name="msgUser"
+          type="text"
+          className="form-control"
+          placeholder="Digite sua mensagem..."
+          onKeyDown={(e) => e.key =="Enter" && handleSubmit()}
+        />
+        <button
+          id="send-button"
+          className="btn btn-light m-0 input-group-text"
+          type="button"
+          onClick={() => handleSubmit()}
+        >
+          <i className="bi bi-send-fill"></i>
+        </button>
+      </div>
     </div>
-))
-}
+  );
+};
 
-        </div>
-        <div id="chat-footer" className='input-group'>
-            <input id="msgUser" name="msgUser" type="text" className='form-control' placeholder='Digite sua mensagem...' />
-            <button id="send-button" className='btn btn-light m-0 input-group-text' type='button'>
-                <i className="bi bi-send-fill"></i>
-            </button>
-        
-          
-        </div>
-
-        </div>
-  )
-}
-
-export default Chat
+export default Chat;
