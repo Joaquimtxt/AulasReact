991075618474545ce5f4506bd595a27e2e0f1a2e
Joaquim <jocaq124@gmail.com>
2025-04-22 08:22:12 -0300

    Início projeto devSteam com react vite

---
+++ 09-devSteam/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.js"
+import "bootstrap-icons/font/bootstrap-icons.css"
+
+function App() {
+
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
