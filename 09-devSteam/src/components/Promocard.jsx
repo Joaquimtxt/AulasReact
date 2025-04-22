9c27f4615798ce863161a100756370b633feafbb
Joaquim <jocaq124@gmail.com>
2025-04-22 12:02:36 -0300

    Feito mais algumas adições em promoCard

--- 09-devSteam/src/components/Promocard.jsx
+++ 09-devSteam/src/components/Promocard.jsx
@@ -5,8 +5,14 @@ const Promocard = (props) => {
     <div className='promoCard card border-0 overflow-hidden'>
         <img className='card-img-top object-fit-cover' src="https://placehold.co/200x200" alt="" />
         <div className='card-body'>
-            <h5 className='card-title'>Título</h5>
-            <p className='card-text'>R$ Preço</p>
+            <h5 className='card-title text-uppercase fw-bold'>Título</h5>
+            <div className='row'>
+                <span className='col-4 text-center bg-success text-success-emphasis fw-bold'>-50%</span>
+                <div className='col card-text bg-dark'>
+            <p className='m-0 p-0 text-end text-light'>R$ Preço</p>
+            <p className='m-0 p-0 text-end text-light'>R$ Preço</p>
+            </div>
+            </div>
             <button className='btn btn-success w-100'>
                 <i className='bi bi-cart-plus me-2'></i>
             </button>
