d8b2317884a22c51d991a9c12afb56a96b79768c
Joaquim <jocaq124@gmail.com>
2025-04-22 08:52:06 -0300

    Criado Header e importado no App, mudado o lugar de import do bootstrap(De index para Main)

--- 09-devSteam/src/App.jsx
+++ 09-devSteam/src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css"
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.bundle.js"
-import "bootstrap-icons/font/bootstrap-icons.css"
+
+import Header from "./components/Header"
 
 function App() {
 
 
   return (
     <>
+  <Header />
     </>
   )
 }
