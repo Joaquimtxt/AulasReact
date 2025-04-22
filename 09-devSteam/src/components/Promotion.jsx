643a4f6d3b6717f81c0ac5620949d14efbe9ed6e
Joaquim <jocaq124@gmail.com>
2025-04-22 10:55:45 -0300

    Iniciado os itens de promoção

---
+++ 09-devSteam/src/components/Promotion.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Promotion = () => {
+  return (
+    <div id='promotion'>
+        <h2>Promoções</h2>
+        <div id='itensPromo'></div>
+        {/*Inserir os cards de promoção     */}
+    </div>
+  )
+}
+
+export default Promotion
